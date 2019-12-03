1d5136e3f6409699fc6c9db84d5ed1a31abe965b
ellaribeiro8 <ellaribeiro8@gmail.com>
2019-12-03 16:33:41 +0000

    Add pagination to the list
    Add pagination to the list so the user can see all the starships possible.
    1- Add Previous and Next buttons
    2- Handle the buttons click
    3- Try to reuse the main function (getStarshipsData) so the code is cleaner and
    simpler
    4- render the buttons only when they are needed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './App.scss';
 import StarshipsList from '../StarshipsList/StarshipsList';
 
-const ENDPOINT = 'https://swapi.co/api';
+const ENDPOINT = 'https://swapi.co/api/starships/';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
         starships: [],
-        current: ENDPOINT + '/starships/?page=1',
         next: '',
         previous: '',
     }
   }
 
-  getStarshipsData() {
-    fetch(this.state.current)
+  componentDidMount(){
+    this.getStarshipsData(ENDPOINT);
+  }
+
+  getStarshipsData(currentURL) {
+    fetch(currentURL)
       .then(data => data.json())
       .then(data => this.setState({
         starships: data.results,
@@ -25,19 +28,48 @@ class App extends React.Component {
       }));
   }
 
-  componentDidMount(){
-    this.getStarshipsData();
+  getPreviousPage(previousURL) {
+    this.getStarshipsData(previousURL);
   }
 
-  render() {   
+  getNextPage(nextURL) {
+    this.getStarshipsData(nextURL);
+  }
+
+  handlePaginationButtonClick(e) {
+    const previousButton = document.getElementById('previous');
+
+    e.target === previousButton ? this.getPreviousPage(this.state.previous) : this.getNextPage(this.state.next)
+  }
+
+  renderPaginationbuttons() {
+    let pagination = [];
+
+    if(this.state.previous !== null) {
+      pagination.push(
+        <button onClick={this.handlePaginationButtonClick.bind(this)} key='previous' id='previous'>Previous</button>
+      );
+    }
+
+    if(this.state.next !== null) {
+      pagination.push(
+        <button onClick={this.handlePaginationButtonClick.bind(this)} key='next' id='next'>Next</button>
+      );
+    }
+
+    return pagination;
+  }
+
+  render() {
     return (
       <div className="App">
         <StarshipsList 
           starships={this.state.starships}
         />
+        {this.renderPaginationbuttons()}
       </div>
     );
   }
 }
 
-export default App;
+export default App
